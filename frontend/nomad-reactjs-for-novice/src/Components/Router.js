0a5f9827d297a5f4379abd991215bfc1736a7b4c
Taekeun Hwang <tkhwang@gmail.com>
2020-04-09 12:45:52 +0900

    #2 Project setup

---
+++ frontend/nomad-reactjs-for-novice/src/Components/Router.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
+import Home from "Routes/Home"
+import Search from "Routes/Search"
+import TV from "Routes/TV"
+
+export default () => (
+	<Router>
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/tv" exact component={TV} />
+			<Route path="/search" exact component={Search} />
+			<Redirect from="*" to="/" />
+		</Switch>
+	</Router>
+)
