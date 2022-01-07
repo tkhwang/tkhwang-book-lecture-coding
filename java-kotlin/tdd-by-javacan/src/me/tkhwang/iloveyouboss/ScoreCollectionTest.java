10f1dda7fad21e5bb4ca33f3f4c76f32fd4cd9f4
Taekeun Hwang <tkhwang@gmail.com>
2022-01-08 00:38:30 +0900

    Setup environment

---
+++ java-kotlin/tdd-by-javacan/src/me/tkhwang/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,12 @@
+package me.tkhwang.iloveyouboss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreCollectionTest {
+    @Test
+    public void test() {
+
+    }
+}
