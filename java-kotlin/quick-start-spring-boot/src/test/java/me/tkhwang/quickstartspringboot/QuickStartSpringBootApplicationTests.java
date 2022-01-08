7bf2df157ba83b5a65b6f8b2387d2a87985e5239
Taekeun Hwang <tkhwang@gmail.com>
2022-01-08 23:02:32 +0900

    Setup 스프링부트quick start project

---
+++ java-kotlin/quick-start-spring-boot/src/test/java/me/tkhwang/quickstartspringboot/QuickStartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tkhwang.quickstartspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
