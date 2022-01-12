2fb3c8c8db40b07c53ce3f061bc01d958cbd2545
Teddy <tkhwang@gmail.com>
2022-01-12 17:28:25 +0900

    3.1.2.3 내장 톰캣으로 테스트하기

--- java-kotlin/quick-start-spring-boot/chap03/src/test/java/me/tkhwang/demo/PropertiesTest.java
+++ java-kotlin/quick-start-spring-boot/chap03/src/test/java/me/tkhwang/demo/PropertiesTest.java
@@ -1,40 +1,36 @@
 package me.tkhwang.demo;
 
+import me.tkhwang.demo.domain.BoardVO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.env.Environment;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class PropertiesTest {
     @Autowired
-    Environment environment;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void testMethod() {
-        System.out.println(" 이름 = " + environment .getProperty("author.name"));
-        System.out.println(" 나이 = " + environment .getProperty("author.age"));
-        System.out.println(" 국가 = " + environment.getProperty("author.nation"));
-    }
+    private TestRestTemplate testRestTemplate;
 
     @Test
     public void testHello() throws Exception {
-        mockMvc.perform(get("/hello").param("name", "둘리"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello : 둘리"))
-                .andDo(print());
+        String result = testRestTemplate.getForObject("/hello?name=둘리", String.class);
+        assertEquals("Hello : 둘리",  result);
+    }
 
+    @Test
+    public void testGetBoard() throws Exception {
+        BoardVO board = testRestTemplate.getForObject("/getBoard", BoardVO.class);
+        assertEquals("테스터", board.getWriter());
     }
 }
