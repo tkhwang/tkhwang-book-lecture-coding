941454d54c9168d016a72ff09dc933d891f8bce0
Taekeun Hwang <tkhwang@gmail.com>
2022-01-15 17:08:01 +0900

    [querydsl] Setup basic project configuration

---
+++ java-kotlin/quick-start-spring-boot/lecture-inflearn-querydsl/src/main/java/me/tkhwang/lectureinflearnquerydsl/LectureInflearnQuerydslApplication.java
@@ -0,0 +1,13 @@
+package me.tkhwang.lectureinflearnquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureInflearnQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureInflearnQuerydslApplication.class, args);
+    }
+
+}
