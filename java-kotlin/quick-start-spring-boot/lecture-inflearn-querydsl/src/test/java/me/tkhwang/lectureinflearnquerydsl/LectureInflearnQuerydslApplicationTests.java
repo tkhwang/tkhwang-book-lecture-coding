8000ddc942f97351a92c8a0c3cdafd4cecc90cb6
Taekeun Hwang <tkhwang@gmail.com>
2022-01-15 18:24:56 +0900

    [querydsl] 프로젝트 환경설정 > Querydsl 설정과 검증

--- java-kotlin/quick-start-spring-boot/lecture-inflearn-querydsl/src/test/java/me/tkhwang/lectureinflearnquerydsl/LectureInflearnQuerydslApplicationTests.java
+++ java-kotlin/quick-start-spring-boot/lecture-inflearn-querydsl/src/test/java/me/tkhwang/lectureinflearnquerydsl/LectureInflearnQuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package me.tkhwang.lectureinflearnquerydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.tkhwang.lectureinflearnquerydsl.entity.Hello;
+import me.tkhwang.lectureinflearnquerydsl.entity.QHello;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
 @SpringBootTest
 class LectureInflearnQuerydslApplicationTests {
 
+//    @PersistenceContext
+    @Autowired
+    EntityManager em;
+
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
