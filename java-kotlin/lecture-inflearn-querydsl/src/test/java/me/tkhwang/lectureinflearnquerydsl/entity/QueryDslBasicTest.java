3ef4988c3d2a6af0c76465d03820a2b55138bb6b
Taekeun Hwang <tkhwang@gmail.com>
2022-01-22 16:08:15 +0900

    [querydsl] 중급 문법 > 동적 쿼리 - BooleanBuilder 사용

--- java-kotlin/lecture-inflearn-querydsl/src/test/java/me/tkhwang/lectureinflearnquerydsl/entity/QueryDslBasicTest.java
+++ java-kotlin/lecture-inflearn-querydsl/src/test/java/me/tkhwang/lectureinflearnquerydsl/entity/QueryDslBasicTest.java
@@ -1,5 +1,6 @@
 package me.tkhwang.lectureinflearnquerydsl.entity;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -231,5 +232,32 @@ public class QueryDslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    public void dynamicQuery_BooleanBuilder() {
+        String usernameparam = "member1";
+        Integer ageparam = 10;
+
+        List<Member> result = searchMember1(usernameparam, ageparam);
+
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
