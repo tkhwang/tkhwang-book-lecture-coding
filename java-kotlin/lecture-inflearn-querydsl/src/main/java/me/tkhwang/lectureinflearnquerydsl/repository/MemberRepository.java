798724b0d6a747fec56095e647230b88248d8fc8
Taekeun Hwang <tkhwang@gmail.com>
2022-01-23 17:18:53 +0900

    [querydsl] 실무 활용 - 스프링 데이터 JPA와 Querydsl > 스프링 데이터 JPA 리포지토리로 변경

---
+++ java-kotlin/lecture-inflearn-querydsl/src/main/java/me/tkhwang/lectureinflearnquerydsl/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package me.tkhwang.lectureinflearnquerydsl.repository;
+
+import me.tkhwang.lectureinflearnquerydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername(String username);
+}
