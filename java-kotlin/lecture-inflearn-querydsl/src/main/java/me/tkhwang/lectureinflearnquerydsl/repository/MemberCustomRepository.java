f95c830f87c93e78e99e728282b61a55c565a12b
Taekeun Hwang <tkhwang@gmail.com>
2022-01-23 18:13:48 +0900

    [querydsl] 실무 활용 - 스프링 데이터 JPA와 Querydsl > 스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- java-kotlin/lecture-inflearn-querydsl/src/main/java/me/tkhwang/lectureinflearnquerydsl/repository/MemberCustomRepository.java
+++ java-kotlin/lecture-inflearn-querydsl/src/main/java/me/tkhwang/lectureinflearnquerydsl/repository/MemberCustomRepository.java
@@ -2,9 +2,14 @@ package me.tkhwang.lectureinflearnquerydsl.repository;
 
 import me.tkhwang.lectureinflearnquerydsl.dto.MemberSearchCondition;
 import me.tkhwang.lectureinflearnquerydsl.dto.MemberTeamDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface MemberCustomRepository {
     List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable page);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable page);
+
 }
--- java-kotlin/lecture-inflearn-querydsl/src/test/java/me/tkhwang/lectureinflearnquerydsl/repository/MemberJpaRepositoryTest.java
+++ java-kotlin/lecture-inflearn-querydsl/src/test/java/me/tkhwang/lectureinflearnquerydsl/repository/MemberJpaRepositoryTest.java
@@ -1,6 +1,9 @@
 package me.tkhwang.lectureinflearnquerydsl.repository;
 
+import me.tkhwang.lectureinflearnquerydsl.dto.MemberSearchCondition;
+import me.tkhwang.lectureinflearnquerydsl.dto.MemberTeamDto;
 import me.tkhwang.lectureinflearnquerydsl.entity.Member;
+import me.tkhwang.lectureinflearnquerydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +13,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
@@ -52,5 +56,60 @@ class MemberJpaRepositoryTest {
         assertThat(result2).containsExactly(member);
     }
 
+    @Test
+    void searchTest_Builder(){
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+        em.persist(teamA);
+        em.persist(teamB);
 
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("TeamB");
+
+        //when
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        //then
+        assertEquals(result.get(0).getUsername(), "member4");
+    }
+
+    @Test
+    void searchTest_Wherer(){
+        //given
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("TeamB");
+
+        //when
+        List<MemberTeamDto> result = memberJpaRepository.searchByWhere(condition);
+        //then
+        assertEquals(result.get(0).getUsername(), "member4");
+    }
 }
