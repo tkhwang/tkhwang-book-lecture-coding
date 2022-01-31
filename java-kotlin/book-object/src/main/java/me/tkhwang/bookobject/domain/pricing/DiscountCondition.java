1f849bfbd2f4c7e4812e829faedc08ac31cc5769
Taekeun Hwang <tkhwang@gmail.com>
2022-02-01 00:55:16 +0900

    [object] Chapt10. 상속과 코드 재사용

--- java-kotlin/book-object/src/main/java/me/tkhwang/bookobject/domain/pricing/DiscountCondition.java
+++
@@ -1,7 +0,0 @@
-package me.tkhwang.bookobject.domain.pricing;
-
-import me.tkhwang.bookobject.domain.Screening;
-
-public interface DiscountCondition {
-    boolean isSatisfiedBy(Screening screening);
-}
