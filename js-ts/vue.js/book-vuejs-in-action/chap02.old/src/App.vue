48b94cc4994a663455bf17f1fa122c63dee2d8df
Taekeun Hwang <tkhwang@gmail.com>
2022-04-02 18:32:33 +0900

    feat: project started

---
+++ js-ts/vue.js/book-vuejs-in-action/chap02.old/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header>
+      <h1 v-text="sitename"></h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#app',
+  name: 'app',
+  data () {
+    return {
+      sitename: 'Vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
