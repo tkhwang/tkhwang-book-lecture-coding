679fb9354a51a12507aa0a21af859787872dc8c5
Taekeun Hwang <tkhwang@gmail.com>
2022-04-03 15:18:21 +0900

    [vue.js] feat: 인풋 컴포넌트 생성 및 등록

---
+++ js-ts/vue.js/inflearn-learn-vue-typescript/vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" />
+    <button @click="addTodo" type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
