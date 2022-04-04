894913643c26a41782b6cde7c29a87880d4a960c
Taekeun Hwang <tkhwang@gmail.com>
2022-04-04 23:03:11 +0900

    [vue.js] 할 일 추가 API 설계 및 구현

--- js-ts/vue.js/inflearn-learn-vue-typescript/vue-todo/src/App.vue
+++ js-ts/vue.js/inflearn-learn-vue-typescript/vue-todo/src/App.vue
@@ -1,29 +1,53 @@
 <template>
   <div>
-    <h1>Vue todo with typescript</h1>
-    <TodoInput
-      :item="todoText"
-      @input="updateTodoText"
-      @add="addTodo"
-    ></TodoInput>
+    <header>
+      <h1>Vue todo with typescript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <div>
+        <ul>
+          <TodoListItem />
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: any[]) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todoItems));
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) ?? "[]";
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
 
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
+      todoItems: [] as any[],
     };
   },
   methods: {
-    addTodo() {
+    addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value);
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
       this.initTodoText();
     },
     updateTodoText(value: string) {
@@ -32,6 +56,10 @@ export default Vue.extend({
     initTodoText() {
       this.todoText = "";
     },
+    fetchTodoItems() {
+      this.todoText = storage.fetch();
+    },
+    // created() {},
   },
 });
 </script>
