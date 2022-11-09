1a9bd0f5533cf6d7b6b72fb651355e623cb11cc2
tkhwang <tkhwang@gmail.com>
2022-11-09 18:03:07 +0900

    feat: :recycle: add more send message for testing

--- framework/js-ts/node.js/nomad-zoom-clone-coding/src/server.js
+++ framework/js-ts/node.js/nomad-zoom-clone-coding/src/server.js
@@ -17,9 +17,15 @@ const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (socket) => {
   console.log("Connected to Browser ✅");
+
   socket.on("close", () => {
     console.log("Disconnected to Browser ❌");
   });
+
+  socket.on("message", (message) => {
+    console.log(message.toString());
+  });
+
   socket.send("hello!!!");
 });
 
