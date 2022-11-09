e5e14db775bd59cfc248039895d491682a1a9dc3
tkhwang <tkhwang@gmail.com>
2022-11-09 18:51:10 +0900

    feat: :sparkles: add nickname feature

--- framework/js-ts/node.js/nomad-zoom-clone-coding/src/public/js/app.js
+++ framework/js-ts/node.js/nomad-zoom-clone-coding/src/public/js/app.js
@@ -1,14 +1,22 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message:", message.data, message);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.appendChild(li);
 });
 
 socket.addEventListener("close", () => {
@@ -18,6 +26,13 @@ socket.addEventListener("close", () => {
 messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+});
+
+nickForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
   input.value = "";
 });
