acde686d6d160f799fff9ee558df0f0a551bea1a
tkhwang <tkhwang@gmail.com>
2022-11-30 21:51:22 +0900

    feat: :wrench: back and make new directory

--- framework/js-ts/nest.js/nomadcoders-number-eats/nuber-eats-backend.old/src/app.module.ts
+++ framework/js-ts/nest.js/nomadcoders-number-eats/nuber-eats-backend.old/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
