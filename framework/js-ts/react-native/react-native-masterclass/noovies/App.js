45aa4f01e82a445f11d5b0e37d0102aed1d0bb1d
tkhwang <tkhwang@gmail.com>
2022-12-09 17:20:49 +0900

    feat(react-native): :sparkles: add app loading feature

--- framework/js-ts/react-native/react-native-masterclass/noovies/App.js
+++ framework/js-ts/react-native/react-native-masterclass/noovies/App.js
@@ -3,25 +3,15 @@ import React, { useState } from "react";
 import { Text } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
+  const [assets] = useAssets([require("./long-long-ago.jpg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
 
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./long-long-ago.jpg"));
-  };
-
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
+
   return <Text>We are done loading!</Text>;
 }
