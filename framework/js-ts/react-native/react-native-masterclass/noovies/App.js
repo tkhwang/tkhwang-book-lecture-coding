8e1ba52d6dc1b1928491ba54e6d8b2365006bc76
tkhwang <tkhwang@gmail.com>
2022-12-19 15:25:15 +0900

    feat(react-native): :sparkles: add navigation feature

--- framework/js-ts/react-native/react-native-masterclass/noovies/App.js
+++ framework/js-ts/react-native/react-native-masterclass/noovies/App.js
@@ -1,20 +1,39 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native";
 import * as Font from "expo-font";
+import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset, useAssets } from "expo-asset";
+import { Asset } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
-  const [assets] = useAssets([require("./long-long-ago.jpg")]);
-  const [loaded] = Font.useFonts(Ionicons.font);
-
-  if (!assets || !loaded) {
-    return <AppLoading />;
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    await Promise.all([...fonts]);
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
   }
-
   return (
     <NavigationContainer>
       <Tabs />
