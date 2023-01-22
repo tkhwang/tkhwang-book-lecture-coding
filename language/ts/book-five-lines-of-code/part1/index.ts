1e63550b9d8121bbc2cb45ca7b0780772c9aae4b
tkhwang <tkhwang@gmail.com>
2023-01-22 17:13:13 +0900

    refactor(typescript): :recycle: rename back to original name

--- language/ts/book-five-lines-of-code/part1/index.ts
+++ language/ts/book-five-lines-of-code/part1/index.ts
@@ -17,7 +17,7 @@ enum RawTile {
   LOCK2,
 }
 
-interface Tile2 {
+interface Tile {
   isAir(): boolean;
   isFlux(): boolean;
   isUnbreakable(): boolean;
@@ -32,7 +32,7 @@ interface Tile2 {
   isLock2(): boolean;
 }
 
-class Air implements Tile2 {
+class Air implements Tile {
   isAir() {
     return true;
   }
@@ -70,7 +70,7 @@ class Air implements Tile2 {
     return false;
   }
 }
-class Flux implements Tile2 {
+class Flux implements Tile {
   isAir() {
     return false;
   }
@@ -108,7 +108,7 @@ class Flux implements Tile2 {
     return false;
   }
 }
-class Unbreakable implements Tile2 {
+class Unbreakable implements Tile {
   isAir() {
     return false;
   }
@@ -146,7 +146,7 @@ class Unbreakable implements Tile2 {
     return false;
   }
 }
-class Player implements Tile2 {
+class Player implements Tile {
   isAir() {
     return false;
   }
@@ -184,7 +184,7 @@ class Player implements Tile2 {
     return false;
   }
 }
-class Stone implements Tile2 {
+class Stone implements Tile {
   isAir() {
     return false;
   }
@@ -222,7 +222,7 @@ class Stone implements Tile2 {
     return false;
   }
 }
-class FallingStone implements Tile2 {
+class FallingStone implements Tile {
   isAir() {
     return false;
   }
@@ -260,7 +260,7 @@ class FallingStone implements Tile2 {
     return false;
   }
 }
-class Box implements Tile2 {
+class Box implements Tile {
   isAir() {
     return false;
   }
@@ -298,7 +298,7 @@ class Box implements Tile2 {
     return false;
   }
 }
-class FallingBox implements Tile2 {
+class FallingBox implements Tile {
   isAir() {
     return false;
   }
@@ -336,7 +336,7 @@ class FallingBox implements Tile2 {
     return false;
   }
 }
-class Key1 implements Tile2 {
+class Key1 implements Tile {
   isAir() {
     return false;
   }
@@ -374,7 +374,7 @@ class Key1 implements Tile2 {
     return false;
   }
 }
-class Lock1 implements Tile2 {
+class Lock1 implements Tile {
   isAir() {
     return false;
   }
@@ -412,7 +412,7 @@ class Lock1 implements Tile2 {
     return false;
   }
 }
-class Key2 implements Tile2 {
+class Key2 implements Tile {
   isAir() {
     return false;
   }
@@ -450,7 +450,7 @@ class Key2 implements Tile2 {
     return false;
   }
 }
-class Lock2 implements Tile2 {
+class Lock2 implements Tile {
   isAir() {
     return false;
   }
@@ -581,7 +581,7 @@ let rawMap: RawTile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
 
-let map: Tile2[][];
+let map: Tile[][];
 
 function assertExhausted(x: never): never {
   throw new Error("Unexpected object: " + x);
