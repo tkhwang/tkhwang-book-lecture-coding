f61c8c3679b17a07b356d07698e693bdc6f6b7b8
tkhwang <tkhwang@gmail.com>
2023-02-06 22:09:35 +0900

    feat(react-native): :sparkles: test main image

--- framework/js-ts/react-native/book-hands-on-react-native/rn-todo/src/App.js
+++ framework/js-ts/react-native/book-hands-on-react-native/rn-todo/src/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import SignInScreen from "./screens/SignInScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
       <Text style={{ fontSize: 30 }}>TODO APP</Text>
+      <SignInScreen />
     </View>
   );
 }
