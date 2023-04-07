cbbe23ae31b728db8b4a972f0249b2cc297753d5
tkhwang <tkhwang@gmail.com>
2023-04-07 23:03:05 +0900

    feat(nest.js): :sparkles: add chapter3 interface

--- framework/js-ts/nest.js/book-backend-programming-by-studing-nestjs/nestjs-app/src/app.module.ts
+++ framework/js-ts/nest.js/book-backend-programming-by-studing-nestjs/nestjs-app/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ApiController } from './api/api.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  controllers: [ApiController, AppController],
   providers: [AppService],
+  imports: [UsersModule],
 })
-export class AppModule {}
+export class AppModule { }
