dcf8855afaa2ae8dded731820399a7b0485e034d
tkhwang <tkhwang@gmail.com>
2023-04-08 15:20:20 +0900

    feat(nest.js): :sparkles: update chapter 6 module

--- framework/js-ts/nest.js/book-backend-programming-by-studing-nestjs/nestjs-app/src/app.module.ts
+++ framework/js-ts/nest.js/book-backend-programming-by-studing-nestjs/nestjs-app/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.env.${process.env.NODE_ENV}`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
