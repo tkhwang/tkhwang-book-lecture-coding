97147b59e54c3427f8353aaa0fe18a5b2dce6f30
tkhwang <tkhwang@gmail.com>
2023-04-08 17:48:16 +0900

    feat(nest.js): :sparkles: handle update and delete request

--- framework/js-ts/nest.js/learn.nestjs.com/iluvcoffee-new/src/coffees/coffees.controller.ts
+++ framework/js-ts/nest.js/learn.nestjs.com/iluvcoffee-new/src/coffees/coffees.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 
@@ -25,5 +27,15 @@ export class CoffeesController {
   create(@Body() body) {
     return body;
     // return `This action creates a coffee`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
