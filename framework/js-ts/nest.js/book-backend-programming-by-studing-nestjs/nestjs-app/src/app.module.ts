ec5b16646a69f5668e0e136ec5ead2b24e591b07
tkhwang <tkhwang@gmail.com>
2023-04-08 15:51:03 +0900

    feat(nest.js): :sparkles: add chapter 7 pipe and validator

--- framework/js-ts/nest.js/book-backend-programming-by-studing-nestjs/nestjs-app/src/app.module.ts
+++ framework/js-ts/nest.js/book-backend-programming-by-studing-nestjs/nestjs-app/src/app.module.ts
@@ -8,7 +8,7 @@ import { UsersModule } from './users/users.module';
   imports: [
     UsersModule,
     ConfigModule.forRoot({
-      envFilePath: [`${__dirname}/config/env/.env.${process.env.NODE_ENV}`],
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       load: [emailConfig],
       isGlobal: true,
       validationSchema,
