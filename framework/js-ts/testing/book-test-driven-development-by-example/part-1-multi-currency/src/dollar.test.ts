b9c4fa6e9de320c4c54f9f3dfa47fa6dbd81b8ed
tkhwang <tkhwang@gmail.com>
2023-05-05 16:30:22 +0900

    test(test): :test_tube: add failing test for dollar class

--- framework/js-ts/testing/book-test-driven-development-by-example/part-1-multi-currency/src/dollar.test.ts
+++ framework/js-ts/testing/book-test-driven-development-by-example/part-1-multi-currency/src/dollar.test.ts
@@ -1,10 +1,13 @@
 import { Dollar } from "./dollar";
 
-describe("test dollar", () => {
-  it("should multiply", () => {
+describe("dollar class", () => {
+  it("should multiply correctly.", () => {
     const five = new Dollar(5);
-    five.times(2);
 
-    expect(five.value).toBe(10);
+    five.times(2);
+    expect(five.amount).toBe(10);
+
+    five.times(3);
+    expect(five.amount).toBe(15);
   });
 });
