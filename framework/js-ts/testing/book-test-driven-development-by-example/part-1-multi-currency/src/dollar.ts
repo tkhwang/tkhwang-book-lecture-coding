941892b16cf84b925b56ad20ef81d1a78e186fe1
tkhwang <tkhwang@gmail.com>
2023-05-05 16:10:26 +0900

    refactor(test): :recycle: refactor to use constructor parameter

--- framework/js-ts/testing/book-test-driven-development-by-example/part-1-multi-currency/src/dollar.ts
+++ framework/js-ts/testing/book-test-driven-development-by-example/part-1-multi-currency/src/dollar.ts
@@ -1,8 +1,5 @@
 export class Dollar {
-  public value: number;
-  constructor(value: number) {
-    this.value = value;
-  }
+  constructor(public value: number) {}
 
   times(num: number) {
     this.value = this.value * num;
