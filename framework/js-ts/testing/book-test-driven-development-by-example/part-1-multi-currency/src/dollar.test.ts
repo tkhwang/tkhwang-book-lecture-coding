98d548ee9111e39dc385e3d92d4894a616e8d5a6
tkhwang <tkhwang@gmail.com>
2023-05-06 16:11:19 +0900

    test(test): :test_tube: add failing Franc test

--- framework/js-ts/testing/book-test-driven-development-by-example/part-1-multi-currency/src/dollar.test.ts
+++ framework/js-ts/testing/book-test-driven-development-by-example/part-1-multi-currency/src/dollar.test.ts
@@ -18,3 +18,12 @@ describe("dollar class", () => {
     }).toThrowError(Error);
   });
 });
+
+describe("franc class", () => {
+  it("multiply() should work correctly.", () => {
+    const five = new Franc(5);
+
+    expect(new Franc(10).equals(five.times(2))).toBe(true);
+    expect(new Franc(15).equals(five.times(3))).toBe(true);
+  });
+});
