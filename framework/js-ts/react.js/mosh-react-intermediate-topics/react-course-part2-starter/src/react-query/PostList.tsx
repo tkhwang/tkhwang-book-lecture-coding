ba77cb9301d0faab07f1dc12869995a897dabe7b
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 05:39:25 +0900

    ✨ feat(react-query): use infinite query

--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/PostList.tsx
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/PostList.tsx
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import usePosts from "./hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
   const pageSize = 10;
   const [page, setPage] = useState(1);
-  const { data: posts, isLoading, error } = usePosts({ page, pageSize });
+  const { data, isLoading, error, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
@@ -15,24 +17,22 @@ const PostList = () => {
     <>
       <select className="mb-3 form-select"></select>
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
       <button
-        disabled={page === 1}
-        className="my-3 btn btn-primary"
-        onClick={() => setPage(page - 1)}
-      >
-        Previous
-      </button>
-      <button
         className="my-3 btn btn-primary ms-1"
-        onClick={() => setPage(page + 1)}
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/usePosts.ts
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,25 +9,27 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) =>
-  useQuery<Post[], Error>({
+  useInfiniteQuery<Post[], Error>({
     // /users/1/posts
     queryKey: ["posts", query],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       axios
         .get("https://jsonplaceholder.typicode.com/posts", {
           params: {
-            _start: (query.page - 1) * query.pageSize,
+            _start: (pageParam - 1) * query.pageSize,
             _limit: query.pageSize,
           },
         })
         .then((res) => res.data),
     staleTime: 1 * 60 * 1000, // 1m
     keepPreviousData: true,
+    getNextPageParam: (lastPages, allPages) => {
+      return lastPages.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 
 export default usePosts;
