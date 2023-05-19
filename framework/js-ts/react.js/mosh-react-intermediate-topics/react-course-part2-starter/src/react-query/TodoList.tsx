b9e268dc29db021960d41fb36a15cebf061925b8
Taekeun Hwang <tkhwang@gmail.com>
2023-05-19 22:51:15 +0900

    ✨ feat(react-query): seperate custom hooks using react-query

--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/TodoList.tsx
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/TodoList.tsx
@@ -1,35 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
 import useTodos from "./hooks/useTodos";
 
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
-
 const TodoList = () => {
-  const {
-    data: todos,
-    error,
-    isLoading,
-  } = useQuery<Todo[], Error>({
-    queryKey: ["todos"],
-    queryFn: () => {
-      return axios
-        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-        .then((res) => res.data);
-    },
-  });
+  const { data: todos, error, isLoading } = useTodos();
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
