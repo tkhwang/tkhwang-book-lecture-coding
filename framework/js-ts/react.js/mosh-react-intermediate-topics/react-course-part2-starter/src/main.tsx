2163fe845a9968b5643e8fa55bea5e02fde4e73e
Taekeun Hwang <tkhwang@gmail.com>
2023-05-19 23:14:39 +0900

    ✨ feat(react-query): customize query settings

--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/main.tsx
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/main.tsx
@@ -4,13 +4,28 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+/*
+{
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300000, // 5m
+      staleTime: 10 * 1000, // 10s
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    },
+  }
+*/
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
