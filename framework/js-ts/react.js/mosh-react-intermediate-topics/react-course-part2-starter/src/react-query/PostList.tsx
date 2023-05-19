2ad3f028f1fd0eec9e9a2227ede23c5198ad25e0
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 05:00:26 +0900

    ✨ feat(react-query): use parameterialized query

--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/PostList.tsx
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/PostList.tsx
@@ -4,19 +4,32 @@ import { useEffect, useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const { data: posts, isLoading, error } = usePosts();
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, isLoading, error } = usePosts(userId);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        className="mb-3 form-select"
+        onChange={(event) => setUserId(parseInt(event.target.value))}
+        value={userId}
+      >
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/usePosts.ts
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/usePosts.ts
@@ -8,15 +8,20 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
   const fetchPosts = () => {
     return axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          userId,
+        },
+      })
       .then((res) => res.data);
   };
 
   return useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    // /users/1/posts
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
     queryFn: fetchPosts,
     staleTime: 1 * 60 * 1000, // 1m
   });
