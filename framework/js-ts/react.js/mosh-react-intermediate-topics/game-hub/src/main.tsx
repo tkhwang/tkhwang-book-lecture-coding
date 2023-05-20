acc202ec90704a8921d308d2c484d6434e167e03
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 16:14:16 +0900

    🐳 chore(react-query): add ReactQueryDevtools

--- framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/main.tsx
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import theme from "./theme";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
