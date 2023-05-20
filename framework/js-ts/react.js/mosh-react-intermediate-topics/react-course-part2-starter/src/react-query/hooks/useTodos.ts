df6f4592678da12248ecf4d8c2a8b17f3118608e
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 15:11:09 +0900

    ✨ feat(react-query): create re-usable api client

--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/useTodos.ts
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/useTodos.ts
@@ -1,6 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
+import APIClient from "../../services/apiClient";
+
+const apiClient = new APIClient<Todo>("/todos");
 
 export interface Todo {
   id: number;
@@ -10,15 +13,9 @@ export interface Todo {
 }
 
 const useTodos = () => {
-  const fetchTodos = () => {
-    return axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-  };
-
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: fetchTodos,
+    queryFn: apiClient.getAll,
     staleTime: 1 * 60 * 1000, // 1m
   });
 };
