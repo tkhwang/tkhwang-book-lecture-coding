f2bbb57cbaef956c31e41a6845b2249b97cc3f7d
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 16:43:10 +0900

    ✨ feat(react-query): implement fetching of platforms

--- framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/hooks/usePlatforms.ts
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/hooks/usePlatforms.ts
@@ -1,11 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
 import platforms from "../data/platforms";
+import { FetchResponse } from "./useData";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/list/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
