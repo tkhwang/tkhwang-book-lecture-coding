db14cab9158e31869cbd9fb799f6a2bbb26eace2
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 15:17:03 +0900

    ✨ feat(react-query): create re-usable HTTP service

--- framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/useTodos.ts
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/useTodos.ts
@@ -2,20 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
 import APIClient from "../../services/apiClient";
-
-const apiClient = new APIClient<Todo>("/todos");
-
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import todoService, { Todo } from "../../services/todoService";
 
 const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: apiClient.getAll,
+    queryFn: todoService.getAll,
     staleTime: 1 * 60 * 1000, // 1m
   });
 };
