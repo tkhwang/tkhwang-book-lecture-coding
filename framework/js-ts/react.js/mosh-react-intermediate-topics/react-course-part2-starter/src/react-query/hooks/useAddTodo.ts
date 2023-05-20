ea5f1b11ae2d308f491dfee7091ad65dfe723e0f
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 14:22:44 +0900

    ✨ feat(react-query): create custom mutation hook

---
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/react-course-part2-starter/src/react-query/hooks/useAddTodo.ts
@@ -0,0 +1,52 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Todo } from "./useTodos";
+import axios from "axios";
+import { CACHE_KEY_TODOS } from "../constants";
+
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
+const useAddTodo = (onAdd: () => void) => {
+  const queryClient = useQueryClient();
+  return useMutation<Todo, Error, Todo, AddTodoContext>({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+    onMutate: (newTodo: Todo) => {
+      const previousTodos =
+        queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [
+        newTodo,
+        ...todos,
+      ]);
+
+      onAdd();
+
+      return { previousTodos };
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      // Approach #1: Invalidating the cache
+      // queryClient.invalidateQueries({
+      //   queryKey: CACHE_KEY_TODOS,
+      // });
+      // Approach #2 : Updating the data in the cache
+      // queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [
+      //   savedTodo,
+      //   ...todos,
+      // ]);
+      // if (ref.current) ref.current.value = "";
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) =>
+        todos.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context.previousTodos);
+    },
+  });
+};
+
+export default useAddTodo;
