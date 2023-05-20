996a25fdf19f42507d2bcd02f57597e64518d552
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 17:12:42 +0900

    ✨ feat(react-query): implement fetching of games

--- framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/hooks/usePlatforms.ts
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/hooks/usePlatforms.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
+import apiClient, { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
-import { FetchResponse } from "./useData";
 
 export interface Platform {
   id: number;
