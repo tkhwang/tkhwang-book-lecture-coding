84f9a26e46bc963bac7a6cdc1dd26fec501fdf12
Taekeun Hwang <tkhwang@gmail.com>
2023-05-20 17:30:54 +0900

    ✨ feat(react-query): create re-usable apiclient

--- framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/hooks/usePlatforms.ts
+++ framework/js-ts/react.js/mosh-react-intermediate-topics/game-hub/src/hooks/usePlatforms.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
 
+const apiClient = new APIClient<Platform>("/platforms/list/parents");
 export interface Platform {
   id: number;
   name: string;
@@ -11,10 +12,7 @@ export interface Platform {
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Platform>>("/platforms/list/parents")
-        .then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
     initialData: { count: platforms.length, results: platforms },
   });
