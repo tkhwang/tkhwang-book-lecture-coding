b18a4bcb313598214d80d2ae215c6c119b565bca
tkhwang <tkhwang@gmail.com>
2024-02-09 15:48:54 +0900

    🧪 test(tdd): add two test cases

--- language/tdd/book-clean-craftsmanship/chapter02/stack.test.ts
+++ language/tdd/book-clean-craftsmanship/chapter02/stack.test.ts
@@ -1,8 +1,14 @@
 import { assert, describe, expect, test } from "vitest"
 import { MyStack } from "./stack"
 
-describe("TDD", () => {
-  test("myStack을 테스트할 수 있다", () => {
+describe("MyStack은", () => {
+  test("stack을 생성할 수 있다", () => {
+    const stack = new MyStack()
+
+    expect(stack.isEmpty()).toBe(true)
+  })
+
+  test("push 할 수 있다", () => {
     const stack = new MyStack()
 
     stack.push(0)
