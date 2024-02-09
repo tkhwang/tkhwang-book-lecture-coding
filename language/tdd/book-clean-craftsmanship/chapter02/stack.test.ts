2f75bbf0c3ec9f223975193213a4e6bcb8d28a31
tkhwang <tkhwang@gmail.com>
2024-02-09 15:49:40 +0900

    🦄 refactor(tdd): move stack creation at first

--- language/tdd/book-clean-craftsmanship/chapter02/stack.test.ts
+++ language/tdd/book-clean-craftsmanship/chapter02/stack.test.ts
@@ -2,15 +2,13 @@ import { assert, describe, expect, test } from "vitest"
 import { MyStack } from "./stack"
 
 describe("MyStack은", () => {
-  test("stack을 생성할 수 있다", () => {
-    const stack = new MyStack()
+  const stack = new MyStack()
 
+  test("stack을 생성할 수 있다", () => {
     expect(stack.isEmpty()).toBe(true)
   })
 
   test("push 할 수 있다", () => {
-    const stack = new MyStack()
-
     stack.push(0)
     expect(stack.isEmpty()).toBe(false)
   })
