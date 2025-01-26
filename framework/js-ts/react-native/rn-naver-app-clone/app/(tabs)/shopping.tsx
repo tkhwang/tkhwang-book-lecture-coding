675a9e08c18c84ca2eb786fc1294d85fb2ca78cb
tkhwang <tkhwang@gmail.com>
2025-01-26 16:08:03 +0900

    ✨ feat(react-native/ShoppingScreen): enhance webview with navigation controls
    - url now relies on a constant `SHOPPING_HOME_URL`
    - add vertical and horizontal scroll indicators
    - add conditional routing for different url schemes
    - create and apply styles for safe area flex, including platform-specific padding

--- framework/js-ts/react-native/rn-naver-app-clone/app/(tabs)/shopping.tsx
+++ framework/js-ts/react-native/rn-naver-app-clone/app/(tabs)/shopping.tsx
@@ -1,12 +1,41 @@
-import { SafeAreaView, View } from 'react-native';
+import { router } from 'expo-router';
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
+
+const SHOPPING_HOME_URL = 'https://shopping.naver.com/home';
 
 function ShoppingScreen() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <WebView source={{ uri: 'https://shopping.naver.com/ns/home' }} style={{ flex: 1 }} />
+    <SafeAreaView style={styles.safearea}>
+      <WebView
+        source={{ uri: SHOPPING_HOME_URL }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        onShouldStartLoadWithRequest={request => {
+          if (request.url?.startsWith(SHOPPING_HOME_URL) || request.mainDocumentURL?.startsWith(SHOPPING_HOME_URL)) {
+            return false;
+          }
+
+          if (request.url?.startsWith('https://')) {
+            router.navigate({
+              pathname: 'browser',
+              params: { initialUrl: request.url },
+            });
+            return false;
+          }
+
+          return true;
+        }}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safearea: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
 
 export default ShoppingScreen;
