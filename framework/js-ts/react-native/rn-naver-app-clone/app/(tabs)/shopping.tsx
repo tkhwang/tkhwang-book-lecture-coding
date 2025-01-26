3fc4a1eb01d9eac6a20f6c8a680f566713894427
tkhwang <tkhwang@gmail.com>
2025-01-26 16:41:32 +0900

    ✨ feat(react-native/ShoppingScreen): add pull-to-refresh functionality to WebView
    - change shopping home URL
    - refactor WebView inside ScrollView to enable pull-to-refresh
    - add loading state for WebView
    - adjust navigation logic within onShouldStartLoadWithRequest callback

--- framework/js-ts/react-native/rn-naver-app-clone/app/(tabs)/shopping.tsx
+++ framework/js-ts/react-native/rn-naver-app-clone/app/(tabs)/shopping.tsx
@@ -1,32 +1,55 @@
 import { router } from 'expo-router';
-import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { useCallback, useRef, useState } from 'react';
+import { Platform, RefreshControl, SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const SHOPPING_HOME_URL = 'https://shopping.naver.com/home';
+const SHOPPING_HOME_URL = 'https://shopping.naver.com/ns/home';
 
 function ShoppingScreen() {
+  const webViewRef = useRef<WebView | null>(null);
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    if (webViewRef.current !== null) {
+      setRefreshing(true);
+      webViewRef.current.reload();
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.safearea}>
-      <WebView
-        source={{ uri: SHOPPING_HOME_URL }}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-        onShouldStartLoadWithRequest={request => {
-          if (request.url?.startsWith(SHOPPING_HOME_URL) || request.mainDocumentURL?.startsWith(SHOPPING_HOME_URL)) {
-            return false;
-          }
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      >
+        <WebView
+          ref={webViewRef}
+          source={{ uri: SHOPPING_HOME_URL }}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          onLoadEnd={() => {
+            setRefreshing(false);
+          }}
+          onShouldStartLoadWithRequest={request => {
+            if (request.url?.startsWith(SHOPPING_HOME_URL) || request.mainDocumentURL?.startsWith(SHOPPING_HOME_URL)) {
+              return true;
+            }
 
-          if (request.url?.startsWith('https://')) {
-            router.navigate({
-              pathname: 'browser',
-              params: { initialUrl: request.url },
-            });
-            return false;
-          }
+            if (request.url?.startsWith('https://')) {
+              router.navigate({
+                pathname: 'browser',
+                params: { initialUrl: request.url },
+              });
+              return false;
+            }
 
-          return true;
-        }}
-      />
+            return true;
+          }}
+          renderLoading={() => <></>}
+          startInLoadingState={true}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
