bfd74070fe428d255f25ff3e11f1bbc6a2b62478
tkhwang <tkhwang@gmail.com>
2025-01-26 15:59:40 +0900

    ✨ feat(react-native/browser): add browser navigation buttons in WebView
    - add 'NavButton' component and implement 'goBack', 'goForward', and 'refresh' functionalities
    - add navigation state indicators ('canGoBack' and 'canGoForward')
    - add 'naver' navigation icon and 'onPress' action to go back to the previous screen
    - add respective styles for the new elements

--- framework/js-ts/react-native/rn-naver-app-clone/app/browser.tsx
+++ framework/js-ts/react-native/rn-naver-app-clone/app/browser.tsx
@@ -1,16 +1,38 @@
-import { useLocalSearchParams } from 'expo-router';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { router, useLocalSearchParams } from 'expo-router';
 import { useMemo, useRef, useState } from 'react';
-import { Animated, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Animated, Platform, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import WebView from 'react-native-webview';
+
+function NavButton({
+  iconName,
+  disabled,
+  onPress,
+}: {
+  iconName: keyof typeof MaterialCommunityIcons.glyphMap;
+  disabled?: boolean;
+  onPress?: () => void;
+}) {
+  return (
+    <TouchableOpacity style={styles.button} disabled={disabled} onPress={onPress}>
+      <MaterialCommunityIcons name={iconName} size={24} color={disabled ? 'gray' : 'white'} />
+    </TouchableOpacity>
+  );
+}
 
 function BrowserScreen() {
   const params = useLocalSearchParams();
   const initialUrl = params.initialUrl as string;
 
+  const webViewRef = useRef<WebView>(null);
+
   const [url, setUrl] = useState(initialUrl);
   const urlTitle = useMemo(() => url.replace('https://', '').split('/')[0], [url]);
 
   const progressAnim = useRef(new Animated.Value(0)).current;
+
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
 
   return (
     <SafeAreaView style={styles.safearea}>
@@ -31,10 +53,14 @@ function BrowserScreen() {
         />
       </View>
       <WebView
+        ref={webViewRef}
         source={{ uri: initialUrl }}
         style={{ flex: 1 }}
         onNavigationStateChange={event => {
+          console.log('TCL: BrowserScreen -> event', event);
           setUrl(event.url);
+          setCanGoBack(event.canGoBack);
+          setCanGoForward(event.canGoForward);
         }}
         onLoadProgress={event => {
           progressAnim.setValue(event.nativeEvent.progress);
@@ -43,6 +69,21 @@ function BrowserScreen() {
           progressAnim.setValue(0);
         }}
       />
+      <View style={styles.navigator}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <View style={styles.naverIconOutline}>
+            <Text style={styles.naverIconText}>N</Text>
+          </View>
+        </TouchableOpacity>
+        <NavButton iconName="arrow-left" disabled={!canGoBack} onPress={() => webViewRef.current?.goBack()} />
+        <NavButton iconName="arrow-right" disabled={!canGoForward} onPress={() => webViewRef.current?.goForward()} />
+        <NavButton iconName="refresh" onPress={() => webViewRef.current?.reload()} />
+      </View>
     </SafeAreaView>
   );
 }
@@ -70,6 +111,32 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     height: '100%',
     width: '50%',
+  },
+  navigator: {
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+  },
+  button: {
+    width: 30,
+    height: 30,
+    padding: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  naverIconOutline: {
+    borderWidth: 1,
+    borderColor: 'white',
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  naverIconText: {
+    color: 'white',
   },
 });
 
