e365d43254219f7ec62d8e449cafd33383c3692d
tkhwang <tkhwang@gmail.com>
2025-01-27 17:59:13 +0900

    ✨ feat(fullstackAI/comp/navigation): enhance navigation menu
    - rename `menu` to `menus`
    - refactor menu item to include nested routing functionality
    - update "Wemake" to "wemake"
    - update styles
    - refactor the information displayed for each menu item

--- framework/js-ts/remix/nomad-maker/app/common/components/navigation.tsx
+++ framework/js-ts/remix/nomad-maker/app/common/components/navigation.tsx
@@ -1,14 +1,17 @@
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "~/common/components/ui/navigation-menu";
 import { Link } from "react-router";
 import { Separator } from "~/common/components/ui/separator";
-import { NavigationMenuContent } from "@radix-ui/react-navigation-menu";
+import { cn } from "~/lib/utils";
 
-const menu = [
+const menus = [
   {
     name: "Products",
     to: "/products",
@@ -96,7 +99,6 @@ const menu = [
   {
     name: "IdeasGPT",
     to: "/ideas",
-    items: [],
   },
   {
     name: "Teams",
@@ -122,21 +124,55 @@ function Navigation() {
       <div>
         <div className="flex items-center">
           <Link to="/" className="font-bold tracking-tighter text-lg">
-            Wemake
+            wemake
           </Link>
           <Separator orientation="vertical" className="h-6 mx-4" />
           <NavigationMenu>
             <NavigationMenuList>
-              {menu.map((menu) => (
+              {menus.map((menu) => (
                 <NavigationMenuItem key={menu.name}>
-                  <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    {menu.items?.map((item) => (
-                      <NavigationMenuItem key={item.name}>
-                        <Link to={item.to}>{item.name}</Link>
-                      </NavigationMenuItem>
-                    ))}
-                  </NavigationMenuContent>
+                  {menu.items ? (
+                    <>
+                      <Link to={menu.to}>
+                        <NavigationMenuTrigger>
+                          {menu.name}
+                        </NavigationMenuTrigger>
+                      </Link>
+                      <NavigationMenuContent>
+                        <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                          {menu.items?.map((item) => (
+                            <NavigationMenuItem
+                              key={item.name}
+                              className={cn([
+                                "select-none rounded-md transition-colors focus:bg-accent hover:bg-accent",
+                                (item.to === "/products/promote" ||
+                                  item.to === "/jobs/submit") &&
+                                  "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                              ])}
+                            >
+                              <NavigationMenuLink asChild>
+                                <Link
+                                  className="p-3 space-y-1 block leading-none no-underline outline-none"
+                                  to={item.to}
+                                >
+                                  <span className="text-sm font-medium leading-none">
+                                    {item.name}
+                                  </span>
+                                  <p className="text-sm leading-snug text-muted-foreground">
+                                    {item.description}
+                                  </p>
+                                </Link>
+                              </NavigationMenuLink>
+                            </NavigationMenuItem>
+                          ))}
+                        </ul>
+                      </NavigationMenuContent>
+                    </>
+                  ) : (
+                    <Link className={navigationMenuTriggerStyle()} to={menu.to}>
+                      {menu.name}
+                    </Link>
+                  )}
                 </NavigationMenuItem>
               ))}
             </NavigationMenuList>
