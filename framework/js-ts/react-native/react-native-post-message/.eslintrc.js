89d0c65e76692fad70470001ead8deafe26b310a
tkhwang <tkhwang@gmail.com>
2025-01-29 16:30:05 +0900

    🔧 config(eslint,prettier): extend rules and add Prettier config
    - add "plugin:import/recommended" and "plugin:import/typescript" to ESLint extends array
    - add "import" to ESLint plugins array
    - add rules for import/order and newlines-between
    - enable error checks for import duplicates, unresolved imports, and import cycles
    - add import resolver settings for TypeScript and Node
    - add Prettier configuration file with specific style settings
    - include "eslint-plugin-import" in dev dependencies of package.json

--- framework/js-ts/react-native/react-native-post-message/.eslintrc.js
+++ framework/js-ts/react-native/react-native-post-message/.eslintrc.js
@@ -1,9 +1,36 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
+  extends: ["expo", "prettier", "plugin:import/recommended", "plugin:import/typescript"],
   ignorePatterns: ["/dist/*"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "import"],
   rules: {
     "prettier/prettier": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin", // Node.js 내장 모듈
+          "external", // npm 패키지
+          "internal", // 프로젝트 내부 모듈
+          ["parent", "sibling"], // 상위 및 형제 디렉토리
+          "index", // 현재 디렉토리
+          "type", // 타입 임포트
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "import/no-duplicates": "error",
+    "import/no-unresolved": "error",
+    "import/no-cycle": "error",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
   },
 };
