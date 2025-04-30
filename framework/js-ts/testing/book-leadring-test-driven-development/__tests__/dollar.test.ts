7000a680bf968ab5f18604e6bfa642b846eca037
tkhwang <tkhwang@gmail.com>
2025-05-01 07:43:42 +0900

    ✨ feat(testing/money): add new Money class and adjust Dollar related tests
    - introduce Money class with amount and currency attributes
    - refactor code and tests to use Money instead of Dollar
    - create new test for Money
    - update times method in Dollar class to return a new Dollar instance

--- framework/js-ts/testing/book-leadring-test-driven-development/__tests__/dollar.test.ts
+++ framework/js-ts/testing/book-leadring-test-driven-development/__tests__/dollar.test.ts
@@ -1,9 +1,12 @@
-import { Dollar } from "../src/dollar";
+import { Money } from "../src/money";
 
 describe("Dollar", () => {
   it("times method 동작해야 한다.", () => {
-    const dollar = new Dollar(5);
-    dollar.times(2);
-    expect(dollar.amount).toBe(10);
+    const dollar = new Money(5, "USD");
+
+    const result = dollar.times(2);
+
+    expect(result.amount).toBe(10);
+    expect(result.currency).toBe("USD");
   });
 });
