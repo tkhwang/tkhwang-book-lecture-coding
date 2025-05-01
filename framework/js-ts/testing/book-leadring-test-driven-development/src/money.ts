c4a7e9949f3116df801feabfad1c9e49385bd76d
tkhwang <tkhwang@gmail.com>
2025-05-01 11:47:47 +0900

    ✨ feat(money): add new currency and division functionality
    - add KRW as new supported currency
    - implement divide method in Money class
    - refactor tests for better readability
    - add 'equals' method to compare Money instances
    - update test cases using 'equals' method for Money comparison

--- framework/js-ts/testing/book-leadring-test-driven-development/src/money.ts
+++ framework/js-ts/testing/book-leadring-test-driven-development/src/money.ts
@@ -1,13 +1,23 @@
 export class Money {
   amount: number;
-  currency: "USD" | "EUR";
+  currency: "USD" | "EUR" | "KRW";
 
-  constructor(amount: number, currency: "USD" | "EUR") {
+  constructor(amount: number, currency: "USD" | "EUR" | "KRW") {
     this.amount = amount;
     this.currency = currency;
   }
 
   times(multiplier: number): Money {
     return new Money(this.amount * multiplier, this.currency);
+  }
+
+  divide(divisor: number): Money {
+    if (divisor === 0) throw new Error("divisor is 0");
+
+    return new Money(this.amount / divisor, this.currency);
+  }
+
+  equals(money: Money): boolean {
+    return this.amount === money.amount && this.currency === money.currency;
   }
 }
