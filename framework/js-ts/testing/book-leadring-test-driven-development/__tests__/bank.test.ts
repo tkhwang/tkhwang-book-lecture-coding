0518163f5eac4e6e8e4c536b16a1702b8fd9f95e
tkhwang <tkhwang@gmail.com>
2025-05-01 14:34:01 +0900

    ✨ feat(testing): add new tests for currency exchange rates
    - set up exchange rates in beforeEach
    - add test for converting from EUR to USD
    - add test for using new value when exchange rate has changed

--- framework/js-ts/testing/book-leadring-test-driven-development/__tests__/bank.test.ts
+++ framework/js-ts/testing/book-leadring-test-driven-development/__tests__/bank.test.ts
@@ -2,6 +2,14 @@ import { Bank } from "../src/bank";
 import { Money } from "../src/money";
 
 describe("Bank", () => {
+  let bank: Bank;
+
+  beforeEach(() => {
+    bank = new Bank();
+    bank.addExchangeRate("EUR", "USD", 1.2);
+    bank.addExchangeRate("USD", "KRW", 1100);
+  });
+
   describe("missing exchange rate", () => {
     it("should throw error", () => {
       const bank = new Bank();
@@ -10,6 +18,25 @@ describe("Bank", () => {
       const expectedError = new Error("EUR->Kalganid");
 
       expect(() => bank.convert(tenEuros, "Kalganid")).toThrow(expectedError);
+    });
+  });
+
+  describe("convert", () => {
+    it("should convert from EUR to USD", () => {
+      const tenEuros = new Money(10, "EUR");
+      expect(bank.convert(tenEuros, "USD").equals(new Money(12, "USD"))).toBe(
+        true
+      );
+    });
+
+    it("should use new value if exchange rate is changed", () => {
+      bank.addExchangeRate("EUR", "USD", 1.3);
+
+      const tenEuros = new Money(10, "EUR");
+
+      expect(bank.convert(tenEuros, "USD").equals(new Money(13, "USD"))).toBe(
+        true
+      );
     });
   });
 });
