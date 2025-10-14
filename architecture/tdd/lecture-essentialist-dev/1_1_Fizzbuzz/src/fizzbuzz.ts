0695ad357c67e4eb3bf61bafb1833aa649879735
tkhwang <tkhwang@gmail.com>
2025-10-14 18:33:51 +0900

    feat(tdd/fizzbuzz): enhance fizzbuzz function and expand test coverage
    - Added validation to throw an error if the input number exceeds 100.
    - Implemented logic to return "Fizz", "Buzz", or "FizzBuzz" based on divisibility by 3 and 5.
    - Expanded test cases to cover various scenarios, including edge cases for input values.

--- architecture/tdd/lecture-essentialist-dev/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ architecture/tdd/lecture-essentialist-dev/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,18 @@
 export const fizzbuzz = (number: number): string => {
+    if (number > 100) {
+        throw new Error("Number must be less than 100");
+    }
+
+    const isDivisibleBy3 = number % 3 === 0;
+    const isDivisibleBy5 = number % 5 === 0;
+
+    if (isDivisibleBy3 && isDivisibleBy5) return "FizzBuzz";
+
+    if (isDivisibleBy3) return "Fizz";
+
+    if (isDivisibleBy5) return "Buzz";
+
+    return String(number);
+
     return String(number);
 };
--- architecture/tdd/lecture-essentialist-dev/1_1_Fizzbuzz/src/index.spec.ts
+++ architecture/tdd/lecture-essentialist-dev/1_1_Fizzbuzz/src/index.spec.ts
@@ -7,4 +7,23 @@ describe("fizzbuzz", () => {
         expect(typeof result).toBe("string");
     })
 
+    // 3 -> Fizz
+    // 5 -> Buzz
+    // 15 -> FizzBuss
+    it.each([
+        { input: 3, output: "Fizz", msg: "divisible by 3" },
+        { input: 6, output: "Fizz", msg: "divisible by 3" },
+        { input: 5, output: "Buzz", msg: "divisible by 5" },
+        { input: 10, output: "Buzz", msg: "divisible by 5" },
+        { input: 15, output: "FizzBuzz", msg: "divisible by 3 and 5" },
+        { input: 30, output: "FizzBuzz", msg: "divisible by 3 and 5" },
+    ])("should return $output if the number is $msg", ({ input, output }) => {
+        const result = fizzbuzz(input);
+        expect(result).toBe(output);
+    })
+
+    it("should throw an error if the number is larger than 100", () => {
+        expect(() => fizzbuzz(101)).toThrow("Number must be less than 100");
+    })
+
 });
