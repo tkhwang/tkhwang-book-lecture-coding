e09d7d90721943fff0f50a01e92bf747c3fb20f4
tkhwang <tkhwang@gmail.com>
2025-10-14 18:24:27 +0900

    feat(tdd/fizzbuzz): implement basic fizzbuzz function and initial test
    - Added fizzbuzz function that converts a number to a string.
    - Created a test case to verify that the fizzbuzz function returns a string type.

--- architecture/tdd/lecture-essentialist-dev/1_1_Fizzbuzz/src/index.spec.ts
+++ architecture/tdd/lecture-essentialist-dev/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,10 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
+    it("should return string type", () => {
+        const result = fizzbuzz(1);
 
+        expect(typeof result).toBe("string");
+    })
 
 });
