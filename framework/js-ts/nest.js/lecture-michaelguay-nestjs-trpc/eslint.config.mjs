319aeda7bfc3532e181d24aca8c0125196b85cea
tkhwang <tkhwang@gmail.com>
2025-11-04 11:46:38 +0900

    chore(nestjs-trpc): update ESLint configuration and add VSCode settings
    - Expanded ESLint ignore patterns to include 'dist/**' and 'node_modules/**'.
    - Modified ESLint rules to disable specific TypeScript checks and adjust unused variable warnings.
    - Introduced a new .vscode/settings.json file to configure editor settings for formatting and linting on save.

--- framework/js-ts/nest.js/lecture-michaelguay-nestjs-trpc/eslint.config.mjs
+++ framework/js-ts/nest.js/lecture-michaelguay-nestjs-trpc/eslint.config.mjs
@@ -6,10 +6,10 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'dist/**', 'node_modules/**'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
@@ -17,7 +17,6 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -26,9 +25,20 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
     },
   },
 );
