1cd474d1aa759f9dea37a83741fa29fe8ea30558
tkhwang <tkhwang@gmail.com>
2025-11-16 15:11:59 +0900

    refactor(tdd/video-rental-test): enhance rental fee and points assertions in tests
    - Updated the assertFeeAndPoint function to accept a Customer object, improving clarity in test cases.
    - Added new test cases for 2-day and 3-day rentals, ensuring correct fee and points calculations for these scenarios.
    - Improved overall test structure for better readability and maintainability.

--- language/tdd/book-clean-craftsmanship/video-rental-test/test/video-rental.test.ts
+++ language/tdd/book-clean-craftsmanship/video-rental-test/test/video-rental.test.ts
@@ -2,8 +2,11 @@ import { describe, it, expect } from 'vitest';
 import { Customer } from '../src/customer.js';
 
 const assertFeeAndPoint = (
-    actualFee: number, actualPoints: number,
+    customer: Customer,
     expectedFee: number, expectedPoints: number) => {
+    const actualFee = customer.getRentalFee();
+    const actualPoints = customer.getRentalPoints();
+
     expect(actualFee).toBe(expectedFee);
     expect(actualPoints).toBe(expectedPoints);
 };
@@ -17,7 +20,28 @@ describe('Video Rental', () => {
             const rentalFee = customer.getRentalFee();
             const rentalPoints = customer.getRentalPoints();
 
-            assertFeeAndPoint(rentalFee, rentalPoints, 1.5, 1);
+            assertFeeAndPoint(customer, 1.5, 1);
         });
+
+        it('2일인 경우 무료이고 포인트는 적립되지 않는다.', () => {
+            const customer = new Customer('John Doe');
+            customer.addRental('Regular', 2);
+
+            const rentalFee = customer.getRentalFee();
+            const rentalPoints = customer.getRentalPoints();
+
+            assertFeeAndPoint(customer, 1.5, 1);
+        })
+
+        it('3일인 경우 무료이고 포인트는 적립되지 않는다.', () => {
+            const customer = new Customer('John Doe');
+            customer.addRental('Regular', 3);
+
+            const rentalFee = customer.getRentalFee();
+            const rentalPoints = customer.getRentalPoints();
+
+            assertFeeAndPoint(customer, 1.5, 1);
+        })
     });
+
 });
